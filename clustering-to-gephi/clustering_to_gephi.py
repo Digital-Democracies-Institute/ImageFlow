f559024880555d77af6d5e13e028a8fc827d663d
Elsa K <elsa.k.donovan@gmail.com>
2021-02-03 12:27:29 -0500

    added clustering-to-gephi

---
+++ clustering-to-gephi/clustering_to_gephi.py
@@ -0,0 +1,109 @@
+import pandas as pd
+import json
+import os
+import numpy as np
+
+# Borrowed method to parse absolute and relative paths
+def splitall(path):
+    allparts = []
+    while 1:
+        parts = os.path.split(path)
+        if parts[0] == path:  # sentinel for absolute paths
+            allparts.insert(0, parts[0])
+            break
+        elif parts[1] == path: # sentinel for relative paths
+            allparts.insert(0, parts[1])
+            break
+        else:
+            path = parts[0]
+            allparts.insert(0, parts[1])
+    return allparts
+
+
+def convert_to_df(file_path):
+    print("Converting to CSV")
+    print(file_path)
+    output = []
+
+    with open(file_path, 'r') as f:
+        for line in f:
+            output_json = json.loads(line)
+            images_in_cluster = output_json['images']
+            cluster = output_json['cluster_no']
+
+            for i in images_in_cluster:
+                #TODO: This will need to be adjusted when used in ComputeCanada
+                socialmedia = splitall(i)
+                socialmedia = socialmedia[5]
+                filename = splitall(i)
+                filename = filename[-1]
+                z = {"cluster": cluster, "FileName": filename, "images": i, "socialmedia": socialmedia}
+
+                output.append(z)
+
+    pd_dataframe = pd.DataFrame(output)
+    pd_dataframe = pd_dataframe.drop_duplicates(subset='images')
+    pd_dataframe.to_csv("clustering.csv")
+    return(pd_dataframe)
+
+def multiplatform_filter(dataframe):
+    print("Filtering out any cluster that does not cross platforms...")
+
+    df2 = dataframe.drop_duplicates(["cluster", "socialmedia"])
+    df = df2.groupby('cluster')['socialmedia'].agg(size= len, set= lambda x: set(x))
+    single_platforms = df[df['size'] == 1]
+
+    single_platforms = pd.DataFrame(single_platforms)
+    cluster_list = single_platforms.index.values.tolist()
+    output = []
+
+    print("Scanning files...")
+
+    output.append(dataframe.loc[dataframe['cluster'].isin(cluster_list)])
+    df2 = pd.DataFrame(np.concatenate(output))
+    df2 = df2.rename(columns={0:"cluster", 1:"FileName", 2:"images", 3:"socialmedia", 4:"URL", 5:"SocialMedia", 6:"BoardName", 7:"group", 8:"hashtags", 9:"UserName", 10:"TimeStamp", 11:"date", 12:"time"})
+    print(df2)
+    return df2
+
+def metadata_merge(df, merge_file):
+    df_merge = pd.read_csv(merge_file)
+    print("Merging files...")
+    output = pd.merge(df, df_merge, on='FileName', how='left')
+    print("Merging at the filename...")
+    output = output[output['socialmedia'].notna()]
+    output = output.drop_duplicates(subset='images')
+    output.to_csv("merge.csv")
+    print(output)
+    return output
+
+def date_check(df):
+    df2 = df['date'].isnull().values.sum()
+    print("Checking if there are any invalid or missing dates...")
+    print("There are " + str(df2) + " files missing dates.")
+
+# def remove_clusters(df):
+#
+
+def main():
+    # file_path = input("Let's get started. Add the filepath of your clustering_output.txt here:")
+    # alternative to manually add file_path here and not use UI
+    file_path = "clustering_output_identicals.txt"
+    pd_dataframe = convert_to_df(file_path)
+    print(pd_dataframe)
+
+    merge = input("Would you like to merge your data with a csv of corresponding metadata? (Yes/No)")
+    if merge == 'Yes':
+        #merge_file = input("What is the filepath to the metadata csv file?")
+        merge_file = '/Volumes/Elsa_HD2/Memes/data/All-Metadata/all_metadata.csv'
+        pd_dataframe = metadata_merge(pd_dataframe, merge_file)
+
+    filter = input("Would you like to filter out for only multi-platform clusters? (Yes/No)")
+    if filter == 'Yes':
+        pd_dataframe = multiplatform_filter(pd_dataframe)
+
+    date_check(pd_dataframe)
+
+
+
+if __name__ == "__main__":
+    main()
