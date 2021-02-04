da483986699c802e43647fef6bf74f2369fdeb28
Elsa K <elsa.k.donovan@gmail.com>
2021-02-03 23:31:34 -0500

    completed script aggregation

--- clustering-to-gephi/clustering_to_gephi.py
+++ clustering-to-gephi/clustering_to_gephi.py
@@ -66,7 +66,7 @@ def multiplatform_filter(dataframe):
 
     output_single.append(dataframe.loc[dataframe['cluster'].isin(single_cluster_list)])
     single_df = pd.DataFrame(np.concatenate(output_single))
-    single_df= single_df.rename(columns={0:"cluster", 1:"FileName", 2:"images", 3:"socialmedia", 4:"URL", 5:"SocialMedia", 6:"BoardName", 7:"group", 8:"hashtags", 9:"UserName", 10:"TimeStamp", 11:"date", 12:"time"})
+    single_df= single_df.rename(columns={0:"cluster", 1:"FileName", 2:"images", 3:"socialmedia", 4:"URL", 5:"SocialMedia", 6:"BoardName", 7:"group", 8:"hashtags", 9:"UserName", 10:"TimeStamp", 11:"date", 12:"time", 13:"datetime"})
     single_df.to_csv("single_platforms.csv", index=False)
 
     multi_cluster_list = multi_platforms.index.values.tolist()
@@ -75,7 +75,7 @@ def multiplatform_filter(dataframe):
     multi_df = pd.DataFrame(np.concatenate(output_multi))
     multi_df = multi_df.rename(
         columns={0: "cluster", 1: "FileName", 2: "images", 3: "socialmedia", 4: "URL", 5: "SocialMedia", 6: "BoardName",
-                 7: "group", 8: "hashtags", 9: "UserName", 10: "TimeStamp", 11: "date", 12: "time"})
+                 7: "group", 8: "hashtags", 9: "UserName", 10: "TimeStamp", 11: "date", 12: "time", 13:"datetime"})
 
     multi_df.to_csv("multi_platforms.csv", index=False)
 
@@ -97,6 +97,25 @@ def date_check(df):
     print("Checking if there are any invalid or missing dates...")
     print("There are " + str(df2) + " files missing dates.")
 
+    rm_dates = input("Would you like to remove files with missing dates? (Yes/No) ")
+    if rm_dates == 'Yes':
+
+        df = pd.DataFrame(df)
+        no_date_df = df[df['date'].isnull()]
+        no_date_df = pd.DataFrame(no_date_df)
+        no_date_df.to_csv("removed_rows_with_no_date.csv", index=False)
+        print("The no_date_df")
+        print(no_date_df)
+
+        df = df[df['date'].notna()]
+        print(df)
+
+        if filter == 'Yes':
+            print("Automatically re-checking for presence of single-platform clusters due to recently deleted rows...")
+            df = multiplatform_filter(df)
+
+    return df
+
 def remove_clusters(df):
     cluster_list = input("Enter the cluster numbers that you would like to remove: ")
     try:
@@ -111,6 +130,73 @@ def remove_clusters(df):
 
     return df
 
+def remove_retweets(df):
+    print("Removing Twitter retweets...")
+
+    df['group'] = df['group'].fillna(df['hashtags'])
+
+    df = pd.DataFrame(df)
+
+    df['datetime'] = pd.to_datetime(df.datetime)
+    #df['time'] = pd.to_datetime(df.time)
+    df['platform_with_groups'] = df['socialmedia'] + "/" + df['group']
+
+    df = df.sort_values(["hashtags","cluster","datetime"], ascending=[True, True, False])
+
+    #df.to_csv("resorted.csv", index=False)
+
+    df.to_csv("added_new_cols.csv", index=False)
+
+    CsvFile = 'added_new_cols.csv'
+    df = pd.read_csv(CsvFile)
+
+    df['next_cluster'] = df.cluster.shift(-1, fill_value=0)
+    df['next_hashtags'] = df.hashtags.shift(-1)
+    # for if cluster is the same and social media changes: add 1 weight.
+    output = []
+
+    count = 0
+    out_count = 0
+    for index, row in df.iterrows():
+        count += 1
+        orig_cluster = int(row['cluster'])
+        next_cluster = int(row['next_cluster'])
+        filename = str(row['FileName'])
+        socialmedia = str(row['socialmedia'])
+        platform_groups = str(row['platform_with_groups'])
+        username = str(row['UserName'])
+        datetime = str(row['datetime'])
+
+
+        orig_hashtags = row['hashtags']
+        next_hashtags = row['next_hashtags']
+
+        #TODO: This is not working for some reason
+        if orig_cluster == next_cluster and orig_hashtags is not next_hashtags:
+            out_count += 1
+            row = {"cluster": orig_cluster, "FileName": filename, "socialmedia": socialmedia,
+                   "platform_with_groups": platform_groups, "hashtags": orig_hashtags, "UserName": username,
+                   "datetime": datetime}
+            #print(row)
+            try:
+                output.append(row)
+            except Exception as ex1:
+                print("unable to append to output")
+        elif orig_cluster != next_cluster:
+            row = {"cluster": orig_cluster, "FileName": filename, "socialmedia": socialmedia,
+                   "platform_with_groups": platform_groups, "hashtags": orig_hashtags, "UserName": username,
+                   "datetime": datetime}
+            try:
+                output.append(row)
+            except Exception as ex1:
+                print("unable to append to output")
+
+    print("count is " + str(out_count))
+    df2 = pd.DataFrame(output)
+    df2 = df2.sort_values(["cluster", "datetime"], ascending=[True, True])
+    print(df2)
+    return df2
+
 
 def main():
     # file_path = input("Let's get started. Add the filepath of your clustering_output.txt here:")
@@ -119,21 +205,29 @@ def main():
     pd_dataframe = convert_to_df(file_path)
     print(pd_dataframe)
 
-    merge = input("Would you like to merge your data with a csv of corresponding metadata? (Yes/No)")
+    merge = input("Would you like to merge your data with a csv of corresponding metadata? (Yes/No) ")
     if merge == 'Yes':
         #merge_file = input("What is the filepath to the metadata csv file?")
         merge_file = '/Volumes/Elsa_HD2/Memes/data/All-Metadata/all_metadata.csv'
         pd_dataframe = metadata_merge(pd_dataframe, merge_file)
 
-    filter = input("Would you like to filter out for only multi-platform clusters? (Yes/No) ")
+    filter = input("Would you like to filter for only multi-platform clusters? (Yes/No) ")
     if filter == 'Yes':
         pd_dataframe = multiplatform_filter(pd_dataframe)
+        print(pd_dataframe)
 
     rm_clusters = input("Would you like to remove specific cluster(s) from the data? (Yes/No) ")
     if rm_clusters == 'Yes':
         pd_dataframe = remove_clusters(pd_dataframe)
 
-    date_check(pd_dataframe)
+    pd_dataframe = date_check(pd_dataframe)
+
+
+    rm_retweets = input("Would you like to remove twitter retweets from the data? (Yes/No) ")
+    if rm_retweets == 'Yes':
+        pd_dataframe = remove_retweets(pd_dataframe)
+
+    pd_dataframe.to_csv("final.csv", index=False)
 
 
 
