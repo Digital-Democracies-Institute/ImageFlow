f5d59559419efd2a2f4c44136076319aba9a5b09
Hedayat Tabesh <htabesh@sfu.ca>
2021-02-11 11:16:18 -0800

    Update image_flow.py

--- image_flow.py
+++ image_flow.py
@@ -7,32 +7,62 @@ import os
 import sys
 sys.path.append('../../')
 from phash import phash
+<<<<<<< HEAD
+from pairwise_comparisons import pairwise_comparisons
+from clustering import clustering
+from visualize import visualize
+
+import time
 
 class image_flow():
     output_path = "/data"
-    input_path = "/Users/hedayattabesh/Documents/Data/raw data/final_dataset/facebook"
+    input_path = "/Users/hedayattabesh/Documents/Data/raw data/final_dataset/"
     identical = False
-    num_workers = 8
+    num_workers = 4
     
     def __init__(self, input_p="", output_p=""):
-        if not input_p == "": input_path = input_p
-        if not output_p == "": input_path = output_p
-        self.phashes = phash(input_p=self.input_path)    
+        if not input_p == "": self.input_path = input_p
+        if not output_p == "": self.output_path = output_p
+        self.phashes = phash(input_p=self.input_path)
+        self.pc = pairwise_comparisons()
+        self.clus = clustering()
+        self.viz = visualize(self.input_path)
+
 
     # step #1: calculate_phashes.py
     def calculate_phashes(self):
         # lets get a list of images first!
         image_list = self.phashes.get_files()
         # now lets calculate there phash!
-        self.phashes.calculate_phash(image_list, num_workers=self.num_workers)
+        return self.phashes.calculate_phash(image_list, num_workers=self.num_workers)
 
     # step #2: pairwise_comparisons.py
+    def calculate_pairwise_comparisons(self, phashes=None):
+        return self.pc.compare(phashes)
 
     # step #3: clustering_phashes.py
+    def calculate_clusters(self, phashes, phashes_diff):
+        return self.clus.cluster(phashes, phashes_diff)
 
-    # step #4: python visualize_clusters_dup.py 
-
+    # step #4: python visualize_clusters_dup.py
+    def visualize_folders(self, clusters=None):
+        self.viz.load_clusters_to_folder(clusters)
+        return
+    
 
 if __name__ == '__main__':
+    start = time.time()
     imfl = image_flow()
-    imfl.calculate_phashes()
+    # print("Calculating Phashes")
+    # phashes = imfl.calculate_phashes()
+    print("Calculating phashes_diff")
+    phashes_diff = imfl.calculate_pairwise_comparisons()
+    # print("Calculating clusters")
+    # clusters = imfl.calculate_clusters(phashes, phashes_diff)
+
+    end = time.time()
+    elapsed_time = end - start
+    print("Elapsed time = " + str(elapsed_time))
+
+    # imfl.visualize_folders()
+
