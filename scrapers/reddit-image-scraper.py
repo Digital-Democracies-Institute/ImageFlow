8ce517d5b77c9745c3811fd4e1adeef6716968a6
Elsa K <elsa.k.donovan@gmail.com>
2021-03-05 16:37:54 -0500

    Update reddit-image-scraper.py

--- scrapers/reddit-image-scraper.py
+++ scrapers/reddit-image-scraper.py
@@ -7,126 +7,129 @@ import pandas as pd
 
 # Easy conversion for date to UNIX timestamps here: https://www.unixtimestamp.com/index.php
 
-# Connect to PushShift API
-try:
-    api = PushshiftAPI()
-except Exception as ex1:
-    print("unable to connect to PushShift")
 
-output = []
-output_df = []
+def scrape_reddit():
 
-#local_path = os.getcwd()
-local_path = "cedar/database/reddit"
-
-# USER INPUT SUBREDDIT LIST AND START AND END DATE CONVERTED TO TIMESTAMP
-sub_list = ['canada', 'canadaleft', 'canadapolitics', 'canadapoliticshumour', 'canadianpolitics', 'lpc', 'metacanada', 'ndp', 'onguardforthee', 'piratepartyofcanada']
-
-after = '1567296000' #Sept 1, 2019
-before = '1575158340' #Nov 30, 2019
-
-
-curr_date = str(datetime.date(datetime.now()))
-curr_date = curr_date.replace('-','')
-
-brokenImageLinks = 0
-counter = 0
-
-for subreddit in sub_list:
-    print("Scraping images in "+str(subreddit)+" begins...")
-    print("...")
-
+    # Connect to PushShift API
     try:
-        meme_list = list(api.search_submissions(subreddit=str(subreddit), filter=['url', 'author', 'created_utc', 'score', 'num_comments'], after=str(after), before=str(before)))
+        api = PushshiftAPI()
+    except Exception as ex1:
+        print("unable to connect to PushShift")
+
+    output = []
+    output_df = []
+
+    #local_path = os.getcwd()
+    local_path = "cedar/database/reddit"
+
+    # USER INPUT SUBREDDIT LIST AND START AND END DATE CONVERTED TO TIMESTAMP
+    sub_list = ['canada', 'canadaleft', 'canadapolitics', 'canadapoliticshumour', 'canadianpolitics', 'lpc', 'metacanada', 'ndp', 'onguardforthee', 'piratepartyofcanada']
+
+    after = '1567296000' #Sept 1, 2019
+    before = '1575158340' #Nov 30, 2019
 
 
-    except Exception as search_Except:
-        print("unable to make query through pushShift")
+    curr_date = str(datetime.date(datetime.now()))
+    curr_date = curr_date.replace('-','')
 
-    #create folders for each subreddit
-    day_dir = local_path + "Reddit/"+str(subreddit)+ "/" + curr_date
+    brokenImageLinks = 0
+    counter = 0
 
-    try:
-        os.mkdir(day_dir)
-    except OSError:
-        print ("Creation of the directory %s failed" % day_dir)
-    else:
-        print ("Successfully created the day directory %s " % day_dir)
+    for subreddit in sub_list:
+        print("Scraping images in "+str(subreddit)+" begins...")
+        print("...")
+
+        try:
+            meme_list = list(api.search_submissions(subreddit=str(subreddit), filter=['url', 'author', 'created_utc', 'score', 'num_comments'], after=str(after), before=str(before)))
 
 
-    for meme in meme_list:
-        #scrapes jpg, gif, and png
-        if (str(meme.url).endswith('.jpg') or str(meme.url).endswith('.gif') or str(meme.url).endswith('.jpeg') or str(meme.url).endswith('.png')):
+        except Exception as search_Except:
+            print("unable to make query through pushShift")
 
-            counter += 1
+        #create folders for each subreddit
+        day_dir = local_path + "Reddit/"+str(subreddit)+ "/" + curr_date
 
-            #and not str(meme.url).startswith('https://i.imgflip.com')
-            print(meme.url)
+        try:
+            os.mkdir(day_dir)
+        except OSError:
+            print ("Creation of the directory %s failed" % day_dir)
+        else:
+            print ("Successfully created the day directory %s " % day_dir)
 
-            redditScore = str(meme.score)
-            numCmts = str(meme.num_comments)
 
-            try:
-                file_name = wget.download(meme.url,day_dir)
+        for meme in meme_list:
+            #scrapes jpg, gif, and png
+            if (str(meme.url).endswith('.jpg') or str(meme.url).endswith('.gif') or str(meme.url).endswith('.jpeg') or str(meme.url).endswith('.png')):
 
-                # ////PUT IN SEPARATE FUNCTION /////
-                unixTime = meme.created_utc
-                timestamp = str(unixTime)
+                counter += 1
 
-                # redditScore and NumComments are added as strings not int
+                #and not str(meme.url).startswith('https://i.imgflip.com')
+                print(meme.url)
+
+                redditScore = str(meme.score)
+                numCmts = str(meme.num_comments)
+
                 try:
-                    #TODO: instead of exiftool tags create DF with tags
-                    row = {"FileName": file_name, "Url": meme.url, "group": subreddit, "timestamp": timestamp}
-
-                    output_df.append(row)
-
-                except Exception as ex1:
-                    print("row creation failed")
-
-
-
-            except Exception as ex:
-                print("Image URL Broken. Could not download.")
-                try:
-                    #Included to fix issue with downloading imgflip images
-                    print("Trying again via Terminal command line...")
-
-                    subprocess.check_call(['wget', meme.url, '-P', day_dir], timeout=60)
-
-                    full_url = meme.url
-                    file_name = full_url.rsplit('/', 1)[-1]
-                    full_path = day_dir + file_name
+                    file_name = wget.download(meme.url,day_dir)
 
                     # ////PUT IN SEPARATE FUNCTION /////
                     unixTime = meme.created_utc
-                    convertDate = datetime.fromtimestamp(int(unixTime)).strftime('%Y-%m-%d')
-                    convertTime = datetime.fromtimestamp(int(unixTime)).strftime('%H:%M:%S')
-                    print(convertDate)
-                    print(convertTime)
-
-                    #get resulting path of that file
-                    #file_name = day_dir +
+                    timestamp = str(unixTime)
 
                     # redditScore and NumComments are added as strings not int
                     try:
-                        #command = "exiftool -RedditPostDate=" + convertDate + " -RedditUser=" + meme.author + " -Subreddit=" + subreddit + " -RedditPostTime=" + convertTime + " -RedditScore=" + redditScore + " -RedditNumCmts=" + numCmts + " " + full_path
-
+                        #TODO: instead of exiftool tags create DF with tags
                         row = {"FileName": file_name, "Url": meme.url, "group": subreddit, "timestamp": timestamp}
 
                         output_df.append(row)
+
                     except Exception as ex1:
-                        print("Dataframe append failed.")
+                        print("row creation failed")
 
-                except (subprocess.CalledProcessError, subprocess.TimeoutExpired) as ex2:
-                    print("Second attempt to download url failed.")
-                    brokenImageLinks+=1
 
-            print("\n")
 
-    #create DF
-     df = pd.DataFrame(output_df)
-    print("...")
+                except Exception as ex:
+                    print("Image URL Broken. Could not download.")
+                    try:
+                        #Included to fix issue with downloading imgflip images
+                        print("Trying again via Terminal command line...")
 
-print("Total Broken Image Links: "+str(brokenImageLinks))
-print("Total attempted image posts: "+str(counter))
+                        subprocess.check_call(['wget', meme.url, '-P', day_dir], timeout=60)
+
+                        full_url = meme.url
+                        file_name = full_url.rsplit('/', 1)[-1]
+                        full_path = day_dir + file_name
+
+                        # ////PUT IN SEPARATE FUNCTION /////
+                        unixTime = meme.created_utc
+                        convertDate = datetime.fromtimestamp(int(unixTime)).strftime('%Y-%m-%d')
+                        convertTime = datetime.fromtimestamp(int(unixTime)).strftime('%H:%M:%S')
+                        print(convertDate)
+                        print(convertTime)
+
+                        #get resulting path of that file
+                        #file_name = day_dir +
+
+                        # redditScore and NumComments are added as strings not int
+                        try:
+                            #command = "exiftool -RedditPostDate=" + convertDate + " -RedditUser=" + meme.author + " -Subreddit=" + subreddit + " -RedditPostTime=" + convertTime + " -RedditScore=" + redditScore + " -RedditNumCmts=" + numCmts + " " + full_path
+
+                            row = {"FileName": file_name, "Url": meme.url, "group": subreddit, "timestamp": timestamp}
+
+                            output_df.append(row)
+                        except Exception as ex1:
+                            print("Dataframe append failed.")
+
+                    except (subprocess.CalledProcessError, subprocess.TimeoutExpired) as ex2:
+                        print("Second attempt to download url failed.")
+                        brokenImageLinks+=1
+
+                print("\n")
+
+        #create DF
+         df = pd.DataFrame(output_df)
+        print("...")
+
+    print("Total Broken Image Links: "+str(brokenImageLinks))
+    print("Total attempted image posts: "+str(counter))
 
