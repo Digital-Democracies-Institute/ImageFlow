d1f789ab9804a641f758cbdc0ad0cb72d932ce71
Elsa K <elsa.k.donovan@gmail.com>
2021-03-05 15:51:52 -0500

    Update 4plebs-image-scraper.py

--- scrapers/4plebs-image-scraper.py
+++ scrapers/4plebs-image-scraper.py
@@ -9,97 +9,99 @@ import pandas as pd
 import csv
 import time
 
-# CHANGE THESE SETTINGS!
-counter = 0
-total_posts = 0
-output_df = []
 
-# USER INPUTS
-start = "2019-10-14" #Sept 1, 2019
-end = '2019-10-21' #Nov 30, 2019
-group = 'pol'   #group aka board_url
-country_code = 'CA' #country code unique to 4chan
+def scrape_4chan(start, end, group, country_code):
 
-current_page = 0
+    # CHANGE THESE SETTINGS!
+    counter = 0
+    total_posts = 0
+    output_df = []
 
-#TODO: What filesystem should we use for our database?
-saveDirectory = "cedar/database/4chan"
-print("The current working directory is " + saveDirectory)
-path = saveDirectory + "/" + group
+    # USER INPUTS
+    # start = "2019-10-14" #Sept 1, 2019
+    # end = '2019-10-21' #Nov 30, 2019
+    # group = 'pol'   #group aka board_url
+    # country_code = 'CA' #country code unique to 4chan
 
-try:
-    os.mkdir(path)
-except OSError:
-    print("Creation of the directory %s failed  - possible directory already exists" % path)
-else:
-    print("Successfully created the directory %s" % path)
+    current_page = 0
+
+    #TODO: What filesystem should we use for our database?
+    saveDirectory = "cedar/database/4chan"
+    print("The current working directory is " + saveDirectory)
+    path = saveDirectory + "/" + group
+
+    try:
+        os.mkdir(path)
+    except OSError:
+        print("Creation of the directory %s failed  - possible directory already exists" % path)
+    else:
+        print("Successfully created the directory %s" % path)
 
 
-# Get the 4chan board catalog JSON file and open it
-base_url = "https://archive.4plebs.org/_/api/chan/search/?boards="+ group + "&start="+ start +"&end=" + end + "&country=" + country_code + "&page=" + str(current_page)
-print(base_url)
-
-result = requests.get(base_url)
-result.raise_for_status()
-print(result.text)
-result = json.loads(result.text)["0"]["posts"]
-
-pd_result = pd.json_normalize(result)
-df = pd.DataFrame(pd_result)
-print(df)
-
-while current_page != None:
-
-    base_url = "https://archive.4plebs.org/_/api/chan/search/?boards=" + group + "&start=" + start + "&end=" + end + "&country=" + country_code + "&page=" + str(current_page)
+    # Get the 4chan board catalog JSON file and open it
+    base_url = "https://archive.4plebs.org/_/api/chan/search/?boards="+ group + "&start="+ start +"&end=" + end + "&country=" + country_code + "&page=" + str(current_page)
     print(base_url)
-    time.sleep(20)
 
     result = requests.get(base_url)
     result.raise_for_status()
+    print(result.text)
     result = json.loads(result.text)["0"]["posts"]
 
     pd_result = pd.json_normalize(result)
     df = pd.DataFrame(pd_result)
+    print(df)
 
-    for i in range(0, len(df)):
-        total_posts += 1
-        try:
-            image_url = df['media.media_link'][i]
-            print(image_url)
-        except Exception as ex_media:
-            print("No media.media_link.")
+    while current_page != None:
 
-        try:
-            #download image file to database folder
-            file_name = wget.download(image_url, path)
-            counter += 1
+        base_url = "https://archive.4plebs.org/_/api/chan/search/?boards=" + group + "&start=" + start + "&end=" + end + "&country=" + country_code + "&page=" + str(current_page)
+        print(base_url)
+        time.sleep(20)
+
+        result = requests.get(base_url)
+        result.raise_for_status()
+        result = json.loads(result.text)["0"]["posts"]
+
+        pd_result = pd.json_normalize(result)
+        df = pd.DataFrame(pd_result)
+
+        for i in range(0, len(df)):
+            total_posts += 1
+            try:
+                image_url = df['media.media_link'][i]
+                print(image_url)
+            except Exception as ex_media:
+                print("No media.media_link.")
 
             try:
-                username = df['name'][i]
-                timestamp = df['timestamp'][i]
-                country = df['poster_country_name'][i]
+                #download image file to database folder
+                file_name = wget.download(image_url, path)
+                counter += 1
 
-                filename = df['media.media'][i]
-                group_name = df['board.shortname'][i]
+                try:
+                    username = df['name'][i]
+                    timestamp = df['timestamp'][i]
+                    country = df['poster_country_name'][i]
 
-                row = {"FileName": file_name, "Url": image_url, "group": group_name, "timestamp": timestamp, "country":country}
-                output_df.append(row)
+                    filename = df['media.media'][i]
+                    group_name = df['board.shortname'][i]
 
-            except Exception as ex1:
-                print("Could not retrieve info.")
+                    row = {"FileName": file_name, "Url": image_url, "group": group_name, "timestamp": timestamp, "country":country}
+                    output_df.append(row)
 
-        except Exception as ex:
-            print("URL not found")
+                except Exception as ex1:
+                    print("Could not retrieve info.")
 
-        print(str(counter) + " images successfully downloaded from page " + str(current_page) + ".")
-        print("...")
-        print("Total posts searched: " + str(total_posts))
+            except Exception as ex:
+                print("URL not found")
 
-    current_page += 1
+            print(str(counter) + " images successfully downloaded from page " + str(current_page) + ".")
+            print("...")
+            print("Total posts searched: " + str(total_posts))
 
-# Output is Pandas Dataframe
-df = pd.DataFrame(output_df)
+        current_page += 1
 
-print("Total posts searched: " + str(total_posts))
-print("Script completed")
+    # Output is Pandas Dataframe
+    df = pd.DataFrame(output_df)
 
+    print("Total posts searched: " + str(total_posts))
+    print("Script completed")
